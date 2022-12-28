e7180c63075a03468f5ae0e97bef2adbc7670b73
carrildan <97158875+carrildan@users.noreply.github.com>
2022-12-28 17:31:52 +0100

    created script.js

---
+++ script/script.js
@@ -0,0 +1,11 @@
+//changes the whats app icon size in child1
+function bigImg (x) {
+    x.style.width = "50px";
+}
+
+function smallImg (x) {
+    x.style.width = "40px"
+}
+
+const linkMessage = document.querySelector(".grid-child1-smallerbanner > h4");
+linkMessage.textContent = "Newborn2";
