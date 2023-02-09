9e468b16585a8cd2343cc2035cd8726bf35342a2
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-09 01:46:21 +0100

    added a dropdown menu for the search bar on the footer

--- script/script.js
+++ script/script.js
@@ -1,11 +1,45 @@
-//changes the whats app icon size in child1
-function bigImg (x) {
+//changes whats app icon size in child1
+/*function bigImg (x) {
     x.style.width = "50px";
 }
 
 function smallImg (x) {
     x.style.width = "40px"
+}*/
+
+
+//welcome user message
+/*function UserMessage() { 
+    const header = document.querySelector(".socialmediaicons");
+    const newPara = document.createElement("p");
+    newPara.textContent = "welcome user";
+    //header.appendChild(newPara);
+    newPara.style.width="150px";
+    newPara.style.margin="0 0 0 50px";
+    newPara.style.float="right";
+    newPara.style.color="white";
+    newPara.style.textAlign="center";
+}
+UserMessage();*/
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
-const linkMessage = document.querySelector(".grid-child1-smallerbanner > h4");
-linkMessage.textContent = "Newborn2";
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("searchbar");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+}
