a5b6493836069d44540272779ca0691386cff15a
carrildan <97158875+carrildan@users.noreply.github.com>
2023-03-17 12:03:53 +0100

    updated comments

--- script/script.js
+++ script/script.js
@@ -26,12 +26,11 @@ UserMessage();*/
 //this function opens a menu list of itens
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-    
   }
 
 
 //sort the dropdown menu list in alphabetic order TO DO: FIGURE OUT WHY IT IS NOT WORKING
-function sort() {
+function Sort() {
   var list, i, switching, shouldSwitch;
   div = document.querySelector(".search-block");
   //a = div.getElementsById("a");
@@ -43,6 +42,7 @@ function sort() {
     b = list.getElementsByTagName("li");
     for (i=0; i<(b.length - 1); i++){
       shouldSwitch = false;
+      //innerHtml do not work here because it would return the whole element of a tag
       if (b[i].textContent.toLowerCase() > b[i+1].textContent.toLowerCase()){
         shouldSwitch = true;
         break;
@@ -61,22 +61,21 @@ function sort() {
 //filter the options on the menu list
 function filterFunction() {
     
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("searchbar");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
-    a = div.getElementsByTagName("a");
-    ul = document.getElementById("list1");
-    li = ul.getElementsByTagName("li");
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("searchbar");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  ul = document.getElementById("list1");
+  li = ul.getElementsByTagName("li");
 
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      } else {
-        li[i].style.display = "none";
-      }
-      
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+    } else {
+      li[i].style.display = "none";
     } 
+  } 
 }
 
 
